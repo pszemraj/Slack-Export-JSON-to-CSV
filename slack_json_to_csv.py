d892d07731a61139d15efbad29e6e8c9db5aa628
peter szemraj <peter.szemraj@gmail.com>
2022-01-09 06:39:28 +0100

    :art: format

--- slack_json_to_csv.py
+++ slack_json_to_csv.py
@@ -4,8 +4,7 @@ import re
 from datetime import datetime
 from cleantext import clean
 from tqdm.auto import tqdm
-import pprint as pp
-
+import pandas as pd
 
 
 def remove_string_extras(mytext):
@@ -18,20 +17,24 @@ def corr(s):
     # removes extra spaces
     return re.sub(r"\.(?! )", ". ", re.sub(r" +", " ", s))
 
+
 def handle_annotated_mention(matchobj):
-    if isinstance(matchobj, list): matchobj = "".join(matchobj)
+    if isinstance(matchobj, list):
+        matchobj = "".join(matchobj)
     return "@{}".format((matchobj.group(0)[2:-1]).split("|")[1])
+
 
 def handle_mention(matchobj):
     global user
-    if isinstance(matchobj, list): matchobj = "".join(matchobj)
+    if isinstance(matchobj, list):
+        matchobj = "".join(matchobj)
     matched_user = remove_string_extras(matchobj.group(0))
     return "@{}".format(matched_user)
 
 
 def transform_text(text):
     text = text.replace("<!channel>", "@channel")
-    text = text.replace("&gt;",  ">")
+    text = text.replace("&gt;", ">")
     text = text.replace("&amp;", "&")
     # Handle "<@U0BM1CGQY|the100rabh> has joined the channel"
     text = re.compile("<@U\w+\|[A-Za-z0-9.-_]+>").sub(handle_annotated_mention, text)
@@ -49,21 +52,22 @@ if __name__ == "__main__":
         verbose = False
 
     content_list = []
-    userlist  = []
-    f = open(outcsv_file, 'w')
-    user = {};
-    with open(userjson, 'r', errors='replace') as user_data:
+    userlist = []
+    f = open(outcsv_file, "w")
+    user = {}
+    with open(userjson, "r", errors="replace") as user_data:
         userlist = json.load(user_data)
         try:
             for userdata in userlist:
                 userid = userdata["id"]
                 if "real_name" in userdata and userdata["real_name"]:
                     realname = clean(userdata["real_name"])
-                    if not re.match('.*[a-zA-Z].*', realname) :
+                    if not re.match(".*[a-zA-Z].*", realname):
                         realname = clean(userdata["name"])
                 else:
                     realname = clean(userdata["name"])
-                if verbose: print(f"realname for user {userid} is {realname}")
+                if verbose:
+                    print(f"realname for user {userid} is {realname}")
                 user[userid] = [realname]
         except KeyError as e:
             print(f"KeyError: {e}\n\n User data: {userid}")
@@ -71,16 +75,22 @@ if __name__ == "__main__":
 
     print("\n\tfinished loading user data\n")
     exclude_types = ["bot_message", "channel_join"]
-    csvwriter = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+    csvwriter = csv.writer(
+        f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL, lineterminator="\n"
+    )
     csvwriter.writerow(["ts", "user", "text", "channel"])
     pbar = tqdm(total=len(os.listdir(jsondir)), desc="Processing files")
     for filename in os.listdir(jsondir):
         if filename.endswith(".json"):
-            with open(os.path.join(jsondir, filename), 'r',  errors='replace') as json_data:
+            with open(
+                os.path.join(jsondir, filename), "r", errors="replace"
+            ) as json_data:
                 data = json.load(json_data)
                 for item in data:
 
-                    if "subtype" in item.keys() and any(item["subtype"] == x for x in exclude_types):
+                    if "subtype" in item.keys() and any(
+                        item["subtype"] == x for x in exclude_types
+                    ):
                         continue
                     if "user_profile" in item.keys():
                         _user_prof = item["user_profile"]
@@ -90,15 +100,28 @@ if __name__ == "__main__":
                             user = clean(_user_prof["name"])
                     else:
                         # do the weird lookup they normally do
-                        _user_cur = user[item["user"]] if item["user"] in user else ["unknown"]
+                        _user_cur = (
+                            user[item["user"]] if item["user"] in user else ["unknown"]
+                        )
                         user = clean(_user_cur[0])
-                    ts = datetime.utcfromtimestamp(float(item['ts']))
+                    ts = datetime.utcfromtimestamp(float(item["ts"]))
                     time = clean(ts.strftime("%Y-%m-%d %H:%M:%S"))
                     text = clean(transform_text(item["text"]))
                     channel = clean(item["channel"]) if "channel" in item.keys() else ""
-                    csvwriter.writerow([time.encode('utf-8'),user.encode('utf-8'), text.encode('utf-8'),channel.encode('utf-8')])
+                    csvwriter.writerow(
+                        [
+                            time.encode("utf-8"),
+                            user.encode("utf-8"),
+                            text.encode("utf-8"),
+                            channel.encode("utf-8"),
+                        ]
+                    )
         pbar.update(1)
     pbar.close()
 
+    if verbose:
+
+        df = pd.read_csv(outcsv_file)
+        print(f"A preview of the generated dataframe is:\n{df.head()}")
 
     f.close()
